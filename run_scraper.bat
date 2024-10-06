b5ba2d153d41ac0b1594501f4f5ee184f4aac81c
Carlos Davila <carlos.davila.luelmo@gmail.com>
2024-10-06 11:57:23 +0200

    Remove bat file

--- run_scraper.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set /p pages="Enter the number of pages to scrape (default: 100): "
-set /p output="Enter the output directory (default: data): "
-
-if "%pages%"=="" set pages=100
-if "%output%"=="" set output=data
-
-BorsaScraper.exe --pages %pages% --output "%output%"
-pause
