aac6b379ec66aadf2c7552a3edcff0743341db05
Carlos Dávila Luelmo <cdavila@pagantis.com>
2024-12-22 20:14:45 +0100

    Adjust for automatic release. (#8)
    * Delete build executable workflow
    * Update dependencies
    * Add release workflow
    * Setup entry point for the package

--- borsait/main.py
+++ borsait/main.py
@@ -5,6 +5,7 @@ Python program to scrape obligation data from the borsaitaliana.it page.
 import argparse
 import asyncio
 import datetime
+import sys
 import time
 from pathlib import Path
 
@@ -15,6 +16,7 @@ from rich.progress import Progress
 
 BASE_URL = "https://www.borsaitaliana.it/"
 RELATIVE_PATH = "borsa/obbligazioni/ricerca-avanzata.html#formAndResults"
+SCRAPE_URL = f"{BASE_URL}{RELATIVE_PATH}"
 
 
 async def get_page_data(page_number: int) -> list[list[str]]:
@@ -23,7 +25,7 @@ async def get_page_data(page_number: int) -> list[list[str]]:
         browser = await p.chromium.launch()
         page = await browser.new_page()
 
-        url = f"{BASE_URL}{RELATIVE_PATH}?page={page_number}"
+        url = f"{SCRAPE_URL}?page={page_number}"
         await page.goto(url)
 
         # Wait for the table to be rendered
@@ -46,7 +48,23 @@ async def get_page_data(page_number: int) -> list[list[str]]:
         return [column_names] + data
 
 
-async def scrape_pages(page_range: range, output_dir: Path) -> None:
+async def save_data(all_data: list[list[str]], output_path: Path) -> None:
+    """Saves the scraped data to an excel workbook."""
+    # Flatten the list of data and write to an Excel file
+    data = [row for page_data in all_data for row in page_data]
+
+    # Prepare the excel file
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    column_names = data[0]
+    sheet.append(column_names)  # type: ignore
+    for row in data[1:]:
+        if row != column_names and row != []:
+            sheet.append(row)  # type: ignore
+    workbook.save(output_path)
+
+
+async def run_scrape(page_range: range, output_dir: Path) -> None:
     """
     Runs the whole process: scrapes all the data and adds it to the output file.
     """
@@ -64,51 +82,56 @@ async def scrape_pages(page_range: range, output_dir: Path) -> None:
             all_data.append(data)
             progress.update(task, advance=1)
 
-    # Flatten the list of data and write to an Excel file
-    data = [row for page_data in all_data for row in page_data]
-
-    # Prepare the excel file
-    workbook = openpyxl.Workbook()
-    sheet = workbook.active
-    column_names = data[0]
-    sheet.append(column_names)  # type: ignore
-    for row in data[1:]:
-        if row != column_names and row != []:
-            sheet.append(row)  # type: ignore
-    workbook.save(output_path)
+    await save_data(all_data=all_data, output_path=output_path)
 
     console.print(f"[bold green]Scraping completed. Data saved to {output_path}")
 
 
-async def main():
+async def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        description="Scrape data from a paginated table on a website."
+        description=f"Estrae dati da la tabella paginata sul sito web {SCRAPE_URL}.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
         "--pages",
         default=100,
         type=int,
-        required=True,
-        help="The number of pages to scrape",
+        required=False,
+        help="Il numero di pagine da estrarre",
     )
     parser.add_argument(
         "--output",
-        default=Path("./data"),
         type=str,
         required=True,
-        help="The directory to save the output file",
+        help="La directory dove salvare il file di output",
     )
+    return parser
+
+
+async def async_main():
+    parser = await get_parser()
+
+    # If no arguments are provided, print help and exit
+    if len(sys.argv) == 1:
+        parser.print_help()
+        return
+
     args = parser.parse_args()
-    n_pages = args.pages
+    pages = args.pages
     output_dir = Path(args.output)
 
     # Ensure the output directory exists
     output_dir.mkdir(parents=True, exist_ok=True)
 
     start_time = time.time()
-    await scrape_pages(range(1, n_pages + 1), output_dir)
+    await run_scrape(range(1, pages + 1), output_dir)
     print(f"Scraping completed in {time.time() - start_time:.2f} seconds.")
 
 
+def main():
+    """Synchronous wrapper to the main function."""
+    asyncio.run(async_main())
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
